8c77a6fc24a116ba600fbfa9b0750cc99fd1b6f3
charleslamb212 <charlesandrewlamb@gmail.com>
2022-12-21 10:56:16 -0800

    adds express server and home page that uses layouts, part 0 completed

---
+++ index.js
@@ -0,0 +1,18 @@
+// require packages
+require('dotenv').config()
+const express = require('express')
+
+
+//app config
+const app = express()
+const PORT = process.env.PORT || 8000
+app.set('view engine', 'ejs')
+
+//routes and controllers
+app.get('/', (req,res) => {
+    res.send('<h1>just getting started!</h1>')
+})
+// listen on a port
+app.listen(PORT, () => {
+    console.log(`authenticating users on PORT ${PORT}`)
+})
